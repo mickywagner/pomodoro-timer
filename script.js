cee721fa388e619ca40de701de754a01c0be3373
Michaela Wagner <mickywagner@gmail.com>
2019-11-18 18:13:00 -0700

    hook up timer buttons to countdown

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let countdown;
 let display = document.querySelector('.time')
+let timerBtns = document.querySelectorAll('[data-time]')
 
 function timer(seconds) {
+    clearInterval(countdown)
     const now = Date.now()
     const then = now + seconds * 1000
     displayTimeLeft(seconds)
@@ -20,9 +22,17 @@ function timer(seconds) {
 
 function displayTimeLeft(seconds) {
     let minutes = Math.floor(seconds / 60)
-    seconds = seconds % 60
+    remainderSeconds = seconds % 60
 
-
-    display.textContent = `${minutes < 10 ? '0' + minutes : minutes} : ${seconds < 10 ? '0' + seconds : seconds}`
+    display.textContent = `${minutes < 10 ? '0' : ''}${minutes} : ${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`
+    document.title = display.textContent
 }
+
+timerBtns.forEach(btn => btn.addEventListener('click', startTimer))
+
+function startTimer() {
+    const seconds = parseInt(this.dataset.time)
+    timer(seconds)
+}
+
 
