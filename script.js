67a0cafe40b44cb2dd67b68e69c1d576e371a364
Michaela Wagner <mickywagner@gmail.com>
2019-11-18 18:55:39 -0700

    add reset/start/stop button functionality

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let countdown;
 let display = document.querySelector('.time')
 let timerBtns = document.querySelectorAll('[data-time]')
+let currentTimer;
+let lastTimer;
 
 function timer(seconds) {
+    currentTimer = seconds
     clearInterval(countdown)
     const now = Date.now()
     const then = now + seconds * 1000
@@ -22,17 +25,41 @@ function timer(seconds) {
 
 function displayTimeLeft(seconds) {
     let minutes = Math.floor(seconds / 60)
-    remainderSeconds = seconds % 60
+    let remainderSeconds = seconds % 60
+    let time = `${minutes < 10 ? '0' : ''}${minutes} : ${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`
 
-    display.textContent = `${minutes < 10 ? '0' : ''}${minutes} : ${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`
-    document.title = display.textContent
+    display.textContent = time
+    document.title = time
 }
 
 timerBtns.forEach(btn => btn.addEventListener('click', startTimer))
 
 function startTimer() {
     const seconds = parseInt(this.dataset.time)
+    lastTimer = seconds
     timer(seconds)
 }
 
+const reset = document.querySelector('#reset')
+const start = document.querySelector('#start')
+const stop = document.querySelector('#stop')
 
+reset.addEventListener('click', resetTimer)
+stop.addEventListener('click', pauseTimer)
+start.addEventListener('click', restartTimer)
+
+function resetTimer() {
+    displayTimeLeft(lastTimer)
+    currentTimer = lastTimer
+    clearInterval(countdown)
+}
+
+function pauseTimer() {
+    const timeLeft = display.textContent
+    clearInterval(countdown)
+    currentTimer = (parseInt(timeLeft.split(':')[0] * 60 + parseInt(timeLeft.slice(-2))))
+}
+
+function restartTimer() {
+    timer(currentTimer)
+}
