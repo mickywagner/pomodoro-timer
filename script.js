ca57db0d002157b4ac5cd2bba75325355e29dce9
Michaela Wagner <mickywagner@gmail.com>
2019-11-18 17:40:51 -0700

    add timer and displaytime functions

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-function timer() {
-    let now = new Date() 
-    console.log(now)
+let countdown;
+let display = document.querySelector('.time')
+
+function timer(seconds) {
+    const now = Date.now()
+    const then = now + seconds * 1000
+    displayTimeLeft(seconds)
+    
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        
+        if(secondsLeft < 0) {
+            clearInterval(countdown);
+            return;
+        }
+        displayTimeLeft(secondsLeft)
+    }, 1000)
+    
+} 
+
+function displayTimeLeft(seconds) {
+    let minutes = Math.floor(seconds / 60)
+    seconds = seconds % 60
+
+
+    display.textContent = `${minutes < 10 ? '0' + minutes : minutes} : ${seconds < 10 ? '0' + seconds : seconds}`
 }
 
-timer()
